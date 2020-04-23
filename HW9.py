7e8c9cec257155ca4730e4ef967c866721725d2e
ArminCS97 <armin_felahatpisheh@edu.aua.am>
2020-04-23 15:35:10 +0400

    done

--- HW9.py
+++ HW9.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, minimize_scalar
 from math import e
 
 
@@ -17,7 +17,7 @@ def penalty_method():
         c += 100
 
 
-penalty_method()
+# penalty_method()
 # Below you can see the output of penalty_method()
 """
 The value of min for c = 0 is  [7.22986163e+08 2.58766826e+17]
@@ -53,31 +53,41 @@ The value of min for c = 2900 is  [1.00017241 2.00017244]
 """
 
 
-def barrier_method():
-    for c in range(1, 25):
+def barrier_method(c):
+
+    # for i in range(1, 25):
         def q(x):  # the function q(x1, x2, c)
-            return e ** (x[0] ** 2 + x[1] ** 2) - 1 / c * (1 / (x[0] + x[1] + 4))
+            return e ** (x[0] ** 2 + x[1] ** 2) - (1 / c) * (1 / (x[0] + x[1] + 4))
 
         def dqx0(x):  # dq/dx1 function
-            return 2 * x[0] * e ** (x[0] ** 2 + x[1] ** 2) - 1 / c * (-1 / (x[0] + x[1] + 4) ** 2)
+            return 2 * x[0] * e ** (x[0] ** 2 + x[1] ** 2) - (1 / c) * (-1 / (x[0] + x[1] + 4) ** 2)
 
         def dqx1(x):  # dq/dx2 function
-            return 2 * x[1] * e ** (x[0] ** 2 + x[1] ** 2) - 1 / c * (-1 / (x[0] + x[1] + 4) ** 2)
+            return 2 * x[1] * e ** (x[0] ** 2 + x[1] ** 2) - (1 / c) * (-1 / (x[0] + x[1] + 4) ** 2)
 
         x0 = np.array([0, 0])  # x0 = [0, 0] as the initial point
-        gradient_of_x0 = np.array([dqx0(x0), dqx1(x0)])  # the same as gradient_of_q(x1=0, x2=0)
+        for i in range(4):
+                gradient_of_x0 = np.array([dqx0(x0), dqx1(x0)])  # the same as gradient_of_q(x1=0, x2=0)
 
-        def steepest_descent(a):  # steepest descent function in terms of an a (the same as alpha)
-            return q(x0 - a * gradient_of_x0)
+                def steepest_descent(a):  # steepest descent function in terms of an a (the same as alpha)
+                    return q(x0 - a * gradient_of_x0)
 
-        a0 = minimize(steepest_descent, x0=x0).x  # minimize the steepest descent function to
-        # get the best a (the sane as alpha) this a will be used to estimate the next iteration
-        next_iteration = x0 - a0 * gradient_of_x0  # next iteration according to steepest_descent(a)
-        print(f"The value of min for c = {c} is ", next_iteration)
+                a0 = minimize_scalar(steepest_descent).x
+                # minimize the steepest descent function to
+                # get the best a (the sane as alpha) this a will be used to estimate the next iteration
+                next_iteration = x0 - a0 * gradient_of_x0  # next iteration according to steepest_descent(a)
+
+                x0 = next_iteration
+                if i == 3:
+                    print(f"The value of min for c = {c} is ", next_iteration)
+
 
 
 def q(x):  # the function q(x1, x2, c)
-    return e ** (x[0] ** 2 + x[1] ** 2) - (1 / 10000000) * (1 / (x[0] + x[1] + 4))
+    return e ** (x[0] ** 2 + x[1] ** 2) - (1 / 100000000) * (1 / (x[0] + x[1] + 4))
 
-b = minimize(q, x0=np.array([1, 1]))
-print(b.x)
+
+b = minimize(q, x0=np.array([0, -1]))
+# print(b.x)
+
+barrier_method(9999999999999999999999999999999999999)
